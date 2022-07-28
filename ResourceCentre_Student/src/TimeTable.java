3a24c3a7ffb67cc9117de979fb9d88cdf69ed6fb
Louis Puah <louispyh@gmail.com>
2022-07-28 14:44:26 +0800

    Update Timetable

--- ResourceCentre_Student/src/TimeTable.java
+++ ResourceCentre_Student/src/TimeTable.java
@@ -1,16 +1,95 @@
 
-public class TimeTable extends Item{
-	private String os;
+public class TimeTable{
+	private String tuition;
+	private String duration;
+	private int price;
+	private int start_time;
+	private int end_time;
+	private String mode;
 
-	public TimeTable(String assetTag, String description, String os) {
-		super(assetTag, description);
-		this.os = os;
+	public TimeTable(String tuition, String duration, int price, int start_time, int end_time, String mode) {
+		this.tuition = tuition;
+		this.duration = duration;
+		this.price = price;
+		this.start_time = start_time;
+		this.end_time = end_time;
+		this.mode = mode;
 	}
 
-	public String getOs() {
-		return os;
-	}
 	
+	
+	public String getTuition() {
+		return tuition;
+	}
+
+
+
+	public String getDuration() {
+		return duration;
+	}
+
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+
+	public int getStart_time() {
+		return start_time;
+	}
+
+
+
+	public int getEnd_time() {
+		return end_time;
+	}
+
+
+
+	public String getMode() {
+		return mode;
+	}
+
+
+
+	public void setTuition(String tuition) {
+		this.tuition = tuition;
+	}
+
+
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+
+	public void setStart_time(int start_time) {
+		this.start_time = start_time;
+	}
+
+
+
+	public void setEnd_time(int end_time) {
+		this.end_time = end_time;
+	}
+
+
+
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+
+
+
 	public String toString(){
 		String output = "";
 		// Write your codes here
