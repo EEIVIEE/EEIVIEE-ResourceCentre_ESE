ae93a398f9a1b5517f18c1137a879bcbeb9e5b07
Matthew21028831 <21028831@myrp.edu.sg>
2022-08-04 15:14:17 +0800

    Deleted ResourceCentreStudent

--- ResourceCentre_Student/test/ResourceCentreStudentTest.java
+++ ResourceCentre_Student/test/ResourceCentreStudentTest.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+//
 public class ResourceCentreStudentTest {
 	//prepare test data
 	private Student s1;
